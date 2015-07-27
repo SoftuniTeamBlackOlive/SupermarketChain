75357ad2180a6ab582f4e1ca814c4bfcb906ad8e
borislavtotev <borislav.totev@gmail.com>
2015-07-28 00:48:27 +0300

    testing data added

--- SupermarketChain/SupermarketChain.Data.SqlServer/Migrations/Configuration.cs
+++ SupermarketChain/SupermarketChain.Data.SqlServer/Migrations/Configuration.cs
@@ -28,12 +28,70 @@ namespace SupermarketChain.Data.SqlServer.Migrations
         private void SeedInitialDataForTesting(SupermarketChainDbContext context)
         {
             // TODO: Add more seed data for testing!
-            context
-                .Vendors
-                .Add(new Vendor
+            string[] vendors = { "Kamenitza", "Nestle Sofia Corp.", "Zagorka Corp.", "Targovishte Bottling Company Ltd." };
+
+            foreach (var vendor in vendors)
+            {
+                context
+                    .Vendors
+                    .Add(new Vendor
+                    {
+                        Name = vendor
+                    });                
+            }
+
+            string[] measures = { "liters", "pieces", "kg" };
+
+            foreach (var measure in measures)
+            {
+                context
+                    .Measures
+                    .Add(new Measure
+                    {
+                        Name = measure
+                    });
+            }
+
+            context.SaveChanges();
+
+            Product[] products =
                 {
-                    Name = "Kamenitza"
-                });
+                    new Product
+                    {
+                        VendorId = 3,
+                        Name = "Beer “Zagorka”",
+                        MeasureId = 1,
+                        Price = 0.86m
+                    },
+                    new Product
+                    {
+                        VendorId = 4,
+                        Name = "Vodka “Targovishte”",
+                        MeasureId = 1,
+                        Price = 7.56m                            
+                    },
+                    new Product
+                    {
+                        VendorId = 3,
+                        Name = "Beer “Beck’s”",
+                        MeasureId = 1,
+                        Price = 1.03m                            
+                    },
+                    new Product
+                    {
+                        VendorId = 4,
+                        Name = "Chocolate “Milka”",
+                        MeasureId = 2,
+                        Price = 2.80m                            
+                    }
+                };
+
+            foreach (var product in products)
+            {
+                context
+                    .Products
+                    .Add(product);
+            }
 
             context.SaveChanges();
         }
